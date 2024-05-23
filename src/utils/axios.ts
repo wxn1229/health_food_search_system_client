6f757ae2590bbd9d05ff629992dc640d6886d25a
wxn1229-vivobook <wxn1229@gmail.com>
2024-05-23 16:52:31 +0800

    .env update

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 const axiosInstance = axios.create({
-  baseURL: "https://natural-comic-fawn.ngrok-free.app", // 替換為你的 API 基本 URL
+  // baseURL: "https://natural-comic-fawn.ngrok-free.app", // 替換為你的 API 基本 URL
+  baseURL: "http://localhost:3003", // 替換為你的 API 基本 URL
   headers: { "Content-Type": "application/json" },
   withCredentials: true,
 });
