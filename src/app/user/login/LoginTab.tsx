76d090ec5edb5d26a0787c1587c64525ac7f86ce
wxn1229 <wxn1229@gmail.com>
2024-06-02 03:11:25 +0800

    debug online

--- src/app/user/login/LoginTab.tsx
+++ src/app/user/login/LoginTab.tsx
@@ -220,12 +220,7 @@ export default function LoginTab() {
                         Sign up
                       </Link>
                     </p>
-                    <p className="text-center text-small">
-                      Forget your password?{" "}
-                      <Link size="sm" onPress={() => setSelected("forget")}>
-                        Forget Password
-                      </Link>
-                    </p>
+
                     <div className="flex gap-2 justify-end">
                       <Button fullWidth color="primary" onClick={loginHandler}>
                         Login
@@ -325,36 +320,6 @@ export default function LoginTab() {
                     <div className="flex gap-2 justify-end">
                       <Button fullWidth color="primary" onClick={signupHandler}>
                         Sign up
-                      </Button>
-                    </div>
-                  </form>
-                </Tab>
-                <Tab key="forget" title="Forget Password">
-                  <form className="flex flex-col gap-4 h-[300px]">
-                    <Input
-                      isRequired
-                      label="Email"
-                      placeholder="Enter your email"
-                      type="email"
-                      value={forgetInput.email}
-                      onChange={(e) => {
-                        setForgetInput({
-                          ...forgetInput,
-                          email: e.target.value,
-                        });
-                      }}
-                    />
-
-                    <p className="text-center text-small">
-                      Already have an account?{" "}
-                      <Link size="sm" onPress={() => setSelected("login")}>
-                        Login
-                      </Link>
-                    </p>
-
-                    <div className="flex gap-2 justify-end">
-                      <Button fullWidth color="primary">
-                        Get Password
                       </Button>
                     </div>
                   </form>
