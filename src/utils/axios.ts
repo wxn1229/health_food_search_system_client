1e0bae2fd28c0a8d661b3060cb73754c4a40bd0b
wxn1229 <wxn1229@gmail.com>
2024-06-02 03:09:34 +0800

    debug online

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 const axiosInstance = axios.create({
-  // baseURL: process.env.NEXT_PUBLIC_SERVER_URL, // 替換為你的 API 基本 URL
-  baseURL: "http://localhost:3003", // 替換為你的 API 基本 URL
+  baseURL: process.env.NEXT_PUBLIC_SERVER_URL, // 替換為你的 API 基本 URL
+  // baseURL: "http://localhost:3003", // 替換為你的 API 基本 URL
   headers: { "Content-Type": "application/json" },
   withCredentials: true,
 });
