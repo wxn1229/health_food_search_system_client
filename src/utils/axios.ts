8a7e3dd8985a5273be0f161f98bdb52aec93abc8
wxn1229 <wxn1229@gmail.com>
2024-06-01 22:44:32 +0800

    debug online

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -3,7 +3,7 @@ const axiosInstance = axios.create({
   // baseURL: "https://natural-comic-fawn.ngrok-free.app", // 替換為你的 API 基本 URL
   baseURL: process.env.NEXT_PUBLIC_SERVER_URL, // 替換為你的 API 基本 URL
   headers: { "Content-Type": "application/json" },
-  withCredentials: false,
+  withCredentials: true,
 });
 
 export default axiosInstance;
