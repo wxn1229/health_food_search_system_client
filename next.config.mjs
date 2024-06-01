a50bab4f9928982dc88210ade8f25bfa2545151e
wxn1229 <wxn1229@gmail.com>
2024-06-01 23:51:19 +0800

    debug online

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://natural-comic-fawn.ngrok-free.app/api/:path*", // 代理到你的后端服务器
+      },
+    ];
+  },
+};
 
 export default nextConfig;
